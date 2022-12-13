baac4c584235f488deb8c3ed2d9219aa4b1eb914
Adam Zalewski <adam.zalewski@student.howest.be>
2022-12-13 17:46:29 +0100

    remove line

--- server.js
+++ server.js
@@ -15,5 +15,4 @@ app.use(cors());
 app.get('/genres', (req, res) => {
   res.json(Genre.getAllGenres());
 });
-const GENRES = [{genre_id: 1, genre: 'Action'}, {genre_id: 2, genre: 'Adventure'}, {genre_id: 3, genre: 'Fantasy'}, {genre_id: 4, genre: 'Science Fiction'}, {genre_id: 5, genre: 'Crime'}, {genre_id: 6, genre: 'Drama'}, {genre_id: 7, genre: 'Thriller'}, {genre_id: 8, genre: 'Animation'}, {genre_id: 9, genre: 'Family'}, {genre_id: 10, genre: 'Western'}, {genre_id: 11, genre: 'Comedy'}, {genre_id: 12, genre: 'Romance'}, {genre_id: 13, genre: 'Horror'}, {genre_id: 14, genre: 'Mystery'}, {genre_id: 15, genre: 'History'}, {genre_id: 16, genre: 'War'}, {genre_id: 17, genre: 'Music'}, {genre_id: 18, genre: 'Documentary'}, {genre_id: 19, genre: 'Foreign'}, {genre_id: 20, genre: 'TV Movie'}];
 
